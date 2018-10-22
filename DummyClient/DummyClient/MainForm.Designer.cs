51110012e880b6937ac64c71771c574c4c600a75
SeMyeongJin <tpaud94@gmail.com>
2018-10-22 18:57:54 +0900

    Complete
    Add Interval, Range of Clients

--- DummyClient/DummyClient/MainForm.Designer.cs
+++ DummyClient/DummyClient/MainForm.Designer.cs
@@ -33,9 +33,20 @@
             this.TextBoxIP = new System.Windows.Forms.TextBox();
             this.PortLabel = new System.Windows.Forms.Label();
             this.TextBoxPort = new System.Windows.Forms.TextBox();
+            this.UserLabel = new System.Windows.Forms.Label();
+            this.TextBoxUser = new System.Windows.Forms.TextBox();
             this.ConnectBtn = new System.Windows.Forms.Button();
             this.LabelConnected = new System.Windows.Forms.Label();
             this.DisconnectBtn = new System.Windows.Forms.Button();
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.LabelTestText = new System.Windows.Forms.Label();
+            this.LabelInterval = new System.Windows.Forms.Label();
+            this.TextBoxInterval = new System.Windows.Forms.TextBox();
+            this.LabelRange = new System.Windows.Forms.Label();
+            this.TextBoxMin = new System.Windows.Forms.TextBox();
+            this.LabelRange2 = new System.Windows.Forms.Label();
+            this.TextBoxMax = new System.Windows.Forms.TextBox();
+            this.TestConnectBtn = new System.Windows.Forms.Button();
             this.TextBoxSend = new System.Windows.Forms.TextBox();
             this.SendBtn = new System.Windows.Forms.Button();
             this.ListBoxLog = new System.Windows.Forms.ListBox();
@@ -47,11 +58,13 @@
             this.GroupBox1.Controls.Add(this.TextBoxIP);
             this.GroupBox1.Controls.Add(this.PortLabel);
             this.GroupBox1.Controls.Add(this.TextBoxPort);
+            this.GroupBox1.Controls.Add(this.UserLabel);
+            this.GroupBox1.Controls.Add(this.TextBoxUser);
             this.GroupBox1.Location = new System.Drawing.Point(14, 15);
             this.GroupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.GroupBox1.Name = "GroupBox1";
             this.GroupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.GroupBox1.Size = new System.Drawing.Size(461, 65);
+            this.GroupBox1.Size = new System.Drawing.Size(566, 65);
             this.GroupBox1.TabIndex = 27;
             this.GroupBox1.TabStop = false;
             this.GroupBox1.Text = "더미 클라이언트 설정";
@@ -67,7 +80,7 @@
             // 
             // textBoxIP
             // 
-            this.TextBoxIP.Location = new System.Drawing.Point(100, 25);
+            this.TextBoxIP.Location = new System.Drawing.Point(90, 25);
             this.TextBoxIP.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.TextBoxIP.MaxLength = 15;
             this.TextBoxIP.Name = "TextBoxIP";
@@ -75,28 +88,49 @@
             this.TextBoxIP.TabIndex = 11;
             this.TextBoxIP.Text = "127.0.0.1";
             this.TextBoxIP.WordWrap = false;
-            this.TextBoxPort.TextChanged += new System.EventHandler(this.TextBoxPort_TextChanged);
+            this.TextBoxIP.TextChanged += new System.EventHandler(this.TextBoxIP_TextChanged);
             // 
             // PortLabel
             // 
             this.PortLabel.AutoSize = true;
-            this.PortLabel.Location = new System.Drawing.Point(225, 30);
+            this.PortLabel.Location = new System.Drawing.Point(205, 30);
             this.PortLabel.Name = "PortLabel";
             this.PortLabel.Size = new System.Drawing.Size(90, 25);
             this.PortLabel.TabIndex = 17;
-            this.PortLabel.Text = "Port Number:";
+            this.PortLabel.Text = "Port:";
             // 
             // textBoxPort
             // 
-            this.TextBoxPort.Location = new System.Drawing.Point(320, 25);
+            this.TextBoxPort.Location = new System.Drawing.Point(240, 25);
             this.TextBoxPort.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.TextBoxPort.MaxLength = 5;
             this.TextBoxPort.Name = "TextBoxPort";
-            this.TextBoxPort.Size = new System.Drawing.Size(40, 25);
+            this.TextBoxPort.Size = new System.Drawing.Size(50, 25);
             this.TextBoxPort.TabIndex = 18;
             this.TextBoxPort.Text = "0";
             this.TextBoxPort.WordWrap = false;
             this.TextBoxPort.TextChanged += new System.EventHandler(this.TextBoxPort_TextChanged);
+            // 
+            // UserLabel
+            // 
+            this.UserLabel.AutoSize = true;
+            this.UserLabel.Location = new System.Drawing.Point(310, 30);
+            this.UserLabel.Name = "UserLabel";
+            this.UserLabel.Size = new System.Drawing.Size(70, 25);
+            this.UserLabel.TabIndex = 53;
+            this.UserLabel.Text = "Client Num:";
+            // 
+            // textBoxUser
+            // 
+            this.TextBoxUser.Location = new System.Drawing.Point(395, 25);
+            this.TextBoxUser.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TextBoxUser.MaxLength = 15;
+            this.TextBoxUser.Name = "TextBoxUser";
+            this.TextBoxUser.Size = new System.Drawing.Size(50, 25);
+            this.TextBoxUser.TabIndex = 54;
+            this.TextBoxUser.Text = "1000";
+            this.TextBoxUser.WordWrap = false;
+            this.TextBoxUser.TextChanged += new System.EventHandler(this.TextBoxUser_TextChanged);
             //
             // ConnectBtn
             //
@@ -113,27 +147,123 @@
             // LabelConnected
             // 
             this.LabelConnected.AutoSize = true;
-            this.LabelConnected.Location = new System.Drawing.Point(210, 103);
+            this.LabelConnected.Location = new System.Drawing.Point(10, 103);
             this.LabelConnected.Name = "LabelConnected";
             this.LabelConnected.Size = new System.Drawing.Size(241, 38);
             this.LabelConnected.TabIndex = 40;
-            this.LabelConnected.Text = "서버 접속 처리 상태 : ";
+            this.LabelConnected.Text = "접속 클라이언트 수 : ";
             //
             // DisconnectBtn
             //
             this.DisconnectBtn.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.DisconnectBtn.Location = new System.Drawing.Point(481, 90);
+            this.DisconnectBtn.Location = new System.Drawing.Point(481, 95);
             this.DisconnectBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.DisconnectBtn.Name = "DisconnectBtn";
-            this.DisconnectBtn.Size = new System.Drawing.Size(93, 38);
+            this.DisconnectBtn.Size = new System.Drawing.Size(98, 38);
             this.DisconnectBtn.TabIndex = 29;
             this.DisconnectBtn.Text = "접속 해제";
             this.DisconnectBtn.UseVisualStyleBackColor = true;
             this.DisconnectBtn.Click += new System.EventHandler(this.OnDisconnectBtn);
+            //
+            // GroupBox2
+            //
+            this.GroupBox2.Location = new System.Drawing.Point(14, 141);
+            this.GroupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.GroupBox2.Size = new System.Drawing.Size(571, 115);
+            this.GroupBox2.TabIndex = 49;
+            this.GroupBox2.TabStop = false;
+            this.GroupBox2.Text = "Connection Test";
+            // 
+            // LabelTestText
+            // 
+            this.LabelTestText.AutoSize = true;
+            this.LabelTestText.Location = new System.Drawing.Point(22, 175);
+            this.LabelTestText.Name = "LabelTestText";
+            this.LabelTestText.Size = new System.Drawing.Size(280, 38);
+            this.LabelTestText.TabIndex = 46;
+            this.LabelTestText.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.LabelTestText.Text = "Interval(ms)마다 범위 내 클라이언트의 접속과 해제를 반복합니다.";
+            // 
+            // LabelInterval
+            // 
+            this.LabelInterval.AutoSize = true;
+            this.LabelInterval.Location = new System.Drawing.Point(22, 219);
+            this.LabelInterval.Name = "LabelInterval";
+            this.LabelInterval.Size = new System.Drawing.Size(90, 38);
+            this.LabelInterval.TabIndex = 51;
+            this.LabelInterval.Text = "Interval:";
+            // 
+            // textBoxInterval
+            // 
+            this.TextBoxInterval.Location = new System.Drawing.Point(87, 211);
+            this.TextBoxInterval.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TextBoxInterval.MaxLength = 5;
+            this.TextBoxInterval.Name = "TextBoxInterval";
+            this.TextBoxInterval.Size = new System.Drawing.Size(40, 25);
+            this.TextBoxInterval.TabIndex = 52;
+            this.TextBoxInterval.Text = "10";
+            this.TextBoxInterval.WordWrap = false;
+            this.TextBoxInterval.TextChanged += new System.EventHandler(this.TextBoxInterval_TextChanged);
+            // 
+            // LabelRange
+            // 
+            this.LabelRange.AutoSize = true;
+            this.LabelRange.Location = new System.Drawing.Point(150, 219);
+            this.LabelRange.Name = "LabelRange";
+            this.LabelRange.Size = new System.Drawing.Size(90, 38);
+            this.LabelRange.TabIndex = 55;
+            this.LabelRange.Text = "클라이언트 수 범위:";
+            // 
+            // textBoxMin
+            // 
+            this.TextBoxMin.Location = new System.Drawing.Point(290, 211);
+            this.TextBoxMin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TextBoxMin.MaxLength = 5;
+            this.TextBoxMin.Name = "TextBoxInterval";
+            this.TextBoxMin.Size = new System.Drawing.Size(40, 25);
+            this.TextBoxMin.TabIndex = 56;
+            this.TextBoxMin.Text = "500";
+            this.TextBoxMin.WordWrap = false;
+            this.TextBoxMin.TextChanged += new System.EventHandler(this.TextBoxMin_TextChanged);
+            // 
+            // textBoxMax
+            // 
+            this.TextBoxMax.Location = new System.Drawing.Point(380, 211);
+            this.TextBoxMax.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TextBoxMax.MaxLength = 5;
+            this.TextBoxMax.Name = "TextBoxInterval";
+            this.TextBoxMax.Size = new System.Drawing.Size(40, 25);
+            this.TextBoxMax.TabIndex = 57;
+            this.TextBoxMax.Text = "800";
+            this.TextBoxMax.WordWrap = false;
+            this.TextBoxMax.TextChanged += new System.EventHandler(this.TextBoxMax_TextChanged);
+            // 
+            // LabelRange2
+            // 
+            this.LabelRange2.AutoSize = true;
+            this.LabelRange2.Location = new System.Drawing.Point(350, 219);
+            this.LabelRange2.Name = "LabelRange2";
+            this.LabelRange2.Size = new System.Drawing.Size(90, 38);
+            this.LabelRange2.TabIndex = 58;
+            this.LabelRange2.Text = "~";
+            //
+            // TestConnectBtn
+            //
+            this.TestConnectBtn.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TestConnectBtn.Location = new System.Drawing.Point(481, 205);
+            this.TestConnectBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TestConnectBtn.Name = "TestConnectBtn";
+            this.TestConnectBtn.Size = new System.Drawing.Size(98, 38);
+            this.TestConnectBtn.TabIndex = 47;
+            this.TestConnectBtn.Text = "접속 테스트";
+            this.TestConnectBtn.UseVisualStyleBackColor = true;
+            this.TestConnectBtn.Click += new System.EventHandler(this.OnTestConnectBtn);
             // 
             // TextBoxSend
             // 
-            this.TextBoxSend.Location = new System.Drawing.Point(14, 140);
+            this.TextBoxSend.Location = new System.Drawing.Point(14, 270);
             this.TextBoxSend.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.TextBoxSend.MaxLength = 32;
             this.TextBoxSend.Name = "TextBoxSend";
@@ -145,10 +275,10 @@
             // SendBtn
             // 
             this.SendBtn.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.SendBtn.Location = new System.Drawing.Point(481, 135);
+            this.SendBtn.Location = new System.Drawing.Point(481, 265);
             this.SendBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.SendBtn.Name = "SendBtn";
-            this.SendBtn.Size = new System.Drawing.Size(93, 38);
+            this.SendBtn.Size = new System.Drawing.Size(98, 38);
             this.SendBtn.TabIndex = 39;
             this.SendBtn.Text = "Send Msg";
             this.SendBtn.UseVisualStyleBackColor = true;
@@ -159,24 +289,33 @@
             this.ListBoxLog.FormattingEnabled = true;
             this.ListBoxLog.HorizontalScrollbar = true;
             this.ListBoxLog.ItemHeight = 15;
-            this.ListBoxLog.Location = new System.Drawing.Point(14, 190);
+            this.ListBoxLog.Location = new System.Drawing.Point(14, 310);
             this.ListBoxLog.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.ListBoxLog.Name = "listBoxLog";
-            this.ListBoxLog.Size = new System.Drawing.Size(559, 210);
+            this.ListBoxLog.Size = new System.Drawing.Size(564, 240);
             this.ListBoxLog.TabIndex = 41;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 410);
+            this.ClientSize = new System.Drawing.Size(592, 560);
             this.Controls.Add(this.ConnectBtn);
             this.Controls.Add(this.LabelConnected);
+            this.Controls.Add(this.LabelTestText);
+            this.Controls.Add(this.LabelInterval);
+            this.Controls.Add(this.TextBoxInterval);
+            this.Controls.Add(this.LabelRange);
+            this.Controls.Add(this.TextBoxMin);
+            this.Controls.Add(this.TextBoxMax);
+            this.Controls.Add(this.LabelRange2);
+            this.Controls.Add(this.TestConnectBtn);
             this.Controls.Add(this.DisconnectBtn);
             this.Controls.Add(this.TextBoxSend);
             this.Controls.Add(this.SendBtn);
             this.Controls.Add(this.ListBoxLog);
             this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.GroupBox2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
@@ -196,9 +335,20 @@
         private System.Windows.Forms.TextBox TextBoxIP;
         private System.Windows.Forms.Label PortLabel;
         private System.Windows.Forms.TextBox TextBoxPort;
+        private System.Windows.Forms.Label UserLabel;
+        private System.Windows.Forms.TextBox TextBoxUser;
         private System.Windows.Forms.Button ConnectBtn;
         private System.Windows.Forms.Label LabelConnected;
         private System.Windows.Forms.Button DisconnectBtn;
+        private System.Windows.Forms.GroupBox GroupBox2;
+        private System.Windows.Forms.Label LabelTestText;
+        private System.Windows.Forms.Label LabelInterval;
+        private System.Windows.Forms.TextBox TextBoxInterval;
+        private System.Windows.Forms.Label LabelRange;
+        private System.Windows.Forms.Label LabelRange2;
+        private System.Windows.Forms.TextBox TextBoxMin;
+        private System.Windows.Forms.TextBox TextBoxMax;
+        private System.Windows.Forms.Button TestConnectBtn;
         private System.Windows.Forms.TextBox TextBoxSend;
         private System.Windows.Forms.Button SendBtn;
         private System.Windows.Forms.ListBox ListBoxLog;
--- DummyClient/DummyClient/MainForm.cs
+++ DummyClient/DummyClient/MainForm.cs
@@ -16,9 +16,11 @@ namespace DummyClient
     public partial class MainForm : Form
     {
         TcpClient dummyClient = new TcpClient();
+        TcpClient[] dummys = new TcpClient[10000];
 
         bool isNetworkThreadRunning = false;
         bool isProcessRunning = false;
+        bool isTestRunning = false;
 
         System.Threading.Thread networkReadThread = null;
         System.Threading.Thread networkSendThread = null;
@@ -26,12 +28,23 @@ namespace DummyClient
         Queue<byte[]> sendPacketQueue = new Queue<byte[]>();
 
         System.Windows.Forms.Timer timer;
+        System.Windows.Forms.Timer TestTimer;
 
+        int count = 0;
+        int UserNum = 0;
+        int Min = 0;
+        int Max = 0;
+        int Avg = 0;
 
 
         public MainForm()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                dummys[i] = new TcpClient();
+            }
         }
 
         private void LoadMainForm(object sender, EventArgs e)
@@ -48,7 +61,10 @@ namespace DummyClient
             timer.Interval = 100;
             timer.Start();
 
+            TestConnectBtn.Enabled = false;
             DisconnectBtn.Enabled = false;
+
+            LabelConnected.Text = string.Format("접속 클라이언트 수 : {0}", count);
 
             TextLog.Write("Program Starts", LOG_LEVEL.INFO);
         }
@@ -59,6 +75,11 @@ namespace DummyClient
             isProcessRunning = false;
 
             dummyClient.Close();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                dummys[i].Close();
+            }
         }
 
         private void OnConnectBtn(object sender, EventArgs e)
@@ -67,15 +88,46 @@ namespace DummyClient
 
             int port = Convert.ToInt32(TextBoxPort.Text);
 
+            UserNum = Convert.ToInt32(TextBoxUser.Text);
+
+            for (int i = 0; i < UserNum; i++)
+            {
+                dummys[i].Connect(address, port);
+            }
+
             if (dummyClient.Connect(address, port))
             {
-                LabelConnected.Text = string.Format("{0}. 서버에 접속 중", DateTime.Now);
                 ConnectBtn.Enabled = false;
+                TestConnectBtn.Enabled = true;
                 DisconnectBtn.Enabled = true;
+
+                count = Convert.ToInt32(TextBoxUser.Text);
             }
             else
             {
-                LabelConnected.Text = string.Format("{0}. 서버에 접속 실패", DateTime.Now);
+                return;
+            }
+            TestConnectBtn.ForeColor = Color.Black;
+            LabelConnected.Text = string.Format("접속 클라이언트 수 : {0}", count);
+        }
+
+        private void OnTestConnectBtn(object sender, EventArgs e)
+        {
+            if (TestConnectBtn.ForeColor == Color.Black)
+            {
+                isTestRunning = true;
+                TestConnectBtn.ForeColor = Color.Blue;
+                TestTimer = new System.Windows.Forms.Timer();
+                TestTimer.Tick += new EventHandler(Test);
+                TestTimer.Interval = Convert.ToInt32(TextBoxInterval.Text);
+                TestTimer.Start();
+                return;
+            }
+            if (TestConnectBtn.ForeColor == Color.Blue)
+            {
+                isTestRunning = false;
+                TestConnectBtn.ForeColor = Color.Black;
+                return;
             }
         }
 
@@ -83,6 +135,15 @@ namespace DummyClient
         {
             SetDisconnected();
             dummyClient.Close();
+
+            UserNum = Convert.ToInt32(TextBoxUser.Text);
+
+            for (int i = 0; i < UserNum; i++)
+            {
+                dummys[i].Close();
+            }
+            count = 0;
+            isTestRunning = false;
         }
 
         private void OnSendBtn(object sender, EventArgs e)
@@ -164,7 +225,7 @@ namespace DummyClient
         {
             // 너무 이 작업만 할 수 없으므로 일정 작업 이상을 하면 일단 패스한다.
             int logWorkCount = 0;
-            
+
             while (isProcessRunning)
             {
                 System.Threading.Thread.Sleep(1);
@@ -195,6 +256,54 @@ namespace DummyClient
             }
         }
 
+        private void Test(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            Min = Convert.ToInt32(TextBoxMin.Text);
+            Max = Convert.ToInt32(TextBoxMax.Text);
+            Avg = (Min + Max) / 2;
+
+            while (isTestRunning)
+            {
+                if ( count >= Avg )
+                {
+                    if (dummys[rand.Next(0, 999)].IsConnected())
+                    {
+                        dummys[rand.Next(0, 999)].Close();
+                        count--;
+                    }
+                }
+                if (count > Min || count < Max)
+                {
+                    if (dummys[rand.Next(0, 999)].IsConnected())
+                    {
+                        dummys[rand.Next(0, 999)].Close();
+                        count--;
+                    }
+                    if (!dummys[rand.Next(0, 999)].IsConnected())
+                    {
+                        dummys[rand.Next(0, 999)].Connect("127.0.0.1", 3500);
+                        count++;
+                    }
+                }
+
+                if (count <= Avg )
+                {
+                    if (!dummys[rand.Next(0, 999)].IsConnected())
+                    {
+                        dummys[rand.Next(0, 999)].Connect("127.0.0.1", 3500);
+                        count++;
+                    }
+                }
+                System.Threading.Thread.Sleep(1);
+
+                if (count > 0)
+                    break;
+            }
+            LabelConnected.Text = string.Format("접속 클라이언트 수 : {0}", count);
+        }
+
         public void SetDisconnected()
         {
             if (ConnectBtn.Enabled == false)
@@ -214,6 +323,26 @@ namespace DummyClient
         }
 
         private void TextBoxIP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxUser_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxInterval_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxMin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxMax_TextChanged(object sender, EventArgs e)
         {
 
         }
